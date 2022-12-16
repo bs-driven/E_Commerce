86baf8d271016496cdcdb8648b0981e3b65c382e
Bradley <bradsamsess90@icloud.com>
2022-12-16 10:11:44 -0500

    table reference adds to catergory.js

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,35 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    price: {
+      type:DataTypes.DECIMAL,
+      allowNull: false,
+      validate:{},
+    },
+    stock:{
+      type: DataTypes.INTERGER,
+      allowNull:false,
+      // set defalut to 10
+      // validate:numeric value
+    },
+    category_id:{
+      type: Datatypes.INTERGER,
+      reference: {}
+    }
   },
   {
     sequelize,
